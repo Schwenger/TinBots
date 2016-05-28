072d572e1720966056727a6720f5800c9b2dc068
Maximilian Köhl <mail@koehlma.de>
2016-05-28 21:11:58 +0200

    [project][simulation] update external c code controller

--- virt_proto/src/tincontrol.cpp
+++ virt_proto/src/tincontrol.cpp
@@ -1,5 +1,28 @@
 #include "tincontrol.h"
 
-double test_function() {
-    return 3.1415;
+void Controller::update_sensors(double* proximity, double* ir) {
+    this->motor_left = 1;
+    this->motor_right = 1;
+}
+
+Controller* controller;
+
+Controller* create_controller() {
+    return new Controller;
+}
+
+void delete_controller(Controller* controller) {
+    delete controller;
+}
+
+double get_motor_left(Controller* controller) {
+    return controller->motor_left;
+}
+
+double get_motor_right(Controller* controller) {
+    return controller->motor_right;
+}
+
+void update_sensors(Controller* controller, double* proximity, double* ir) { 
+    controller->update_sensors(proximity, ir);
 }
--- virt_proto/src/tincontrol.h
+++ virt_proto/src/tincontrol.h
@@ -1,1 +1,15 @@
-extern double test_function();
+class Controller {
+public:
+    double motor_left, motor_right;
+    void update_sensors(double* proximity, double* ir);
+};
+
+extern Controller* controller;
+
+extern Controller* create_controller();
+extern void delete_controller(Controller* controller);
+
+extern double get_motor_left(Controller* controller);
+extern double get_motor_right(Controller* controller);
+
+extern void update_sensors(Controller* controller, double* proximity, double* ir);
