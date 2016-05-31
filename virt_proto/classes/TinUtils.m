619c5e1ba986755c69deb352b12cd3febf42c4e4
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2016-05-31 20:41:21 +0200

    [project][simulation] Fix raycasting algorithm

--- virt_proto/classes/TinUtils.m
+++ virt_proto/classes/TinUtils.m
@@ -1,4 +1,13 @@
 classdef TinUtils < handle
+    properties (Constant)
+        step_size = 0.9;
+        % The assumption of centrality (see raycast_to) introduces some
+        % error, so the bounds are:
+        % [-1/sqrt(2) 1/sqrt(2)]
+        error_range = [-1 +1]/sqrt(2);
+        radius = 0;%5.3/2;
+    end
+    
     methods (Static)
         function line = readline(file_handle)
             coder.extrinsic('fgets');
@@ -33,97 +42,85 @@ classdef TinUtils < handle
                         
             show(prm);
         end
-                
-        function [distance, object] = raycast_hv(matrix, row, col, delta_row, delta_col)
-            [rows, cols] = size(matrix);
-            next_row = row + delta_row;
-            next_col = col + delta_col;
-            if (next_row < 1) || (next_row > rows) || (next_col < 1) || (next_col > cols)
-                object = 1;
-                distance = 0;
-            else
-                while matrix(next_row, next_col) == 0
-                    next_row = next_row + delta_row;
-                    next_col = next_col + delta_col;
-                    if (next_row < 1) || (next_row > rows) || (next_col < 1) || (next_col > cols)
-                        next_row = next_row - delta_row;
-                        next_col = next_col - delta_col;
-                        break;
-                    end
-                end
-                object = matrix(next_row, next_col);
-                distance = delta_row * (next_row - row) + delta_col * (next_col - col) - 0.5;
-            end
-        end
         
-        function [distance, object] = raycast_diag(matrix, row, col, delta_row, delta_col)
-            [rows, cols] = size(matrix);
-            row = row + 0.5;
-            col = col + 0.5;
-            next_row = row + delta_row;
-            next_col = col + delta_col;
-            delta_row = delta_row * 2;
-            delta_col = delta_col * 2;
-            if (next_row < 1) || (next_row > rows) || (next_col < 1) || (next_col > cols)
-                object = 1;
+        function is_same = same_square(row, col, next_row, next_col)
+            is_same = floor(next_row) == floor(row) && floor(next_col) == floor(col);
+        end
+
+        function inside = is_inside(pos, rows, cols)
+            inside = (pos(1) >= 1) && (pos(1) < cols + 1) && (pos(2) >= 1) && (pos(2) < rows + 1);
+        end
+
+        function [distance, object] = raycast_to(matrix, pos, phi, to, QP_vd, QP_q)
+            object = matrix(to(2), to(1));
+            if object == 0
                 distance = inf;
             else
-                while matrix(floor(next_row), floor(next_col)) == 0
-                    next_row = next_row + delta_row;
-                    next_col = next_col + delta_col;
-                    if (next_row < 1) || (next_row > rows) || (next_col < 1) || (next_col > cols)
-                        if (next_row < 1)
-                            next_row = 1;
-                        elseif (next_row > rows)
-                            next_row = rows;
-                        end
-                        if (next_col < 1)
-                            next_col = 1;
-                        elseif (next_col > cols)
-                            next_col = cols;
-                        end
+                % Assume the square of the bot is exactly in the middle of the
+                % box.  Maximum error: sqrt(2)*0.5 \approx 0.707 (unbiased)
+                % A square (i, j) represents the 2D interval $$[i, i+1) \times
+                % [j, j+1)$$, with precisely these parens (half-open).
+                distance = intersectQuarterSquare(pos(1)+0.5, pos(2)+0.5, phi, to + QP_vd, QP_q);
+                distance = max(0, distance - TinUtils.radius);
+                if distance >= Inf
+                    object = 0;
+                end
+            end
+        end
+
+        function [distance, object] = raycast(matrix, row, col, phi)
+            step = [cos(phi) sin(phi)] .* TinUtils.step_size;
+            [rows, cols] = size(matrix);
+            cur_pos = [col row];
+            cur_delta = [0.5 0.5];
+            QP_q = sign(step) + (step == 0);
+            QP_vd = -QP_q/2 + 0.5;
+
+            % Step until we hit something
+            while 1
+                % Step until something changes
+                assert(all(cur_delta < 1) && all(cur_delta >= 0));
+                while all(cur_delta < 1) && all(cur_delta >= 0)
+                    cur_delta = cur_delta + step;
+                end
+
+                % Determine what changed
+                assert(all(cur_delta < 2) && all(cur_delta >= -1), 'A step too far');
+                change = sign(floor(cur_delta));
+                assert(sum(abs(change)) <= 2);
+
+                % Check if we WILL (future!) be still inside after
+                % resolving deltas
+                next_pos = cur_pos + change;
+                if ~TinUtils.is_inside(next_pos, rows, cols)
+                    distance = inf;
+                    object = 0;
+                    break;
+                end
+
+                % Determine if there's an object somewhere
+                if change(1)
+                    [distance, object] = TinUtils.raycast_to(matrix, [col row], phi, cur_pos + [change(1) 0], QP_vd, QP_q);
+                    if object
                         break;
                     end
                 end
-                object = matrix(floor(next_row), floor(next_col));
-                distance = sqrt((next_row - row) ^ 2 + (next_col - col) ^ 2);
-            end
-        end
-        
-        function [distance, object] = raycast(matrix, row, col, direction)
-            deg = round(360 * mod(direction, 2 * pi) / (2 * pi));
-            tangens = tan(deg2rad(deg));
-            if (deg == 0)
-                [distance, object] = TinUtils.raycast_hv(matrix, row, col, 0, 1);
-            elseif (deg == 90)
-                [distance, object] = TinUtils.raycast_hv(matrix, row, col, -1, 0);
-            elseif (deg == 180)
-                [distance, object] = TinUtils.raycast_hv(matrix, row, col, 0, -1);
-            elseif (deg == 270)
-                [distance, object] = TinUtils.raycast_hv(matrix, row, col, 1, 0);
-            else
-                if (deg < 90)
-                    delta_col = 0.501;
-                    delta_row = -0.501;
-                elseif (deg < 180);
-                    delta_col = -0.501;
-                    delta_row = -0.501;
-                elseif (deg < 270);
-                    delta_col = -0.501;
-                    delta_row = 0.501;
-                else
-                    delta_col = 0.501;
-                    delta_row = 0.501;
+                if change(2)
+                    [distance, object] = TinUtils.raycast_to(matrix, [col row], phi, cur_pos + [0 change(2)], QP_vd, QP_q);
+                    if object
+                        break;
+                    end
                 end
-                [distance1, object1] = TinUtils.raycast_diag(matrix, row, col, -tangens * delta_col, delta_col);
-                [distance2, object2] = TinUtils.raycast_diag(matrix, row, col, delta_row, -delta_row / tangens);
-                if distance1 < distance2
-                    distance = distance1;
-                    object = object1;
-                else
-                    distance = distance2;
-                    object = object2;
+                if change(1) && change(2)
+                    [distance, object] = TinUtils.raycast_to(matrix, [col row], phi, next_pos, QP_vd, QP_q);
+                    if object
+                        break;
+                    end
                 end
+
+                % Actually move
+                cur_pos = next_pos;
+                cur_delta = cur_delta - change;
             end
         end
     end
--- virt_proto/classes/intersectQuarterSquare.m
+++ virt_proto/classes/intersectQuarterSquare.m
@@ -1,23 +1,18 @@
 %% 'dir' must be in radians
 %% Examples:
-%% assert(  intersectQuarterPlane(0, 0, 0) == 5  )
-%% assert(  intersectQuarterPlane(-1, 0, 0) == 6  )
-%% assert(  intersectQuarterPlane(1, 0, 0) == 4  )
-%% assert(  intersectQuarterPlane(0, 0, -pi/4) <= 5*sqrt(2) + 1e-10  )
-%% assert(isinf(  intersectQuarterPlane(0, 0, +pi/4)   ))
-function dist = intersectQuarterPlane(x, y, dir)
+%% assert(  intersectQuarterSquare(0, 0, 0) == 5  )
+%% assert(  intersectQuarterSquare(-1, 0, 0) == 6  )
+%% assert(  intersectQuarterSquare(1, 0, 0) == 4  )
+%% assert(  intersectQuarterSquare(0, 0, -pi/4) <= 5*sqrt(2) + 1e-10  )
+%% assert(isinf(  intersectQuarterSquare(0, 0, +pi/4)   ))
+function dist = intersectQuarterSquare(x, y, dir, QP_v, QP_q)
 
-% Specify quarter-plane:
+% Specify quarter-square:
 % QP_v is the pivotal vertex, QP_q sppecifies the quadrant
-QP_v = [5 3];
-QP_q = [+1 -1];
-% Note that this is equivalent to all other quarter-planes, so there's
-% no need to change this.  Instead, change the start position/direction
-% of the robot
 
-% Essential idea: if it hits the plane, then it has to hit at least one
+% Essential idea: if it hits the square, then it has to hit at least one
 % of the (full-)lines where one of the intersection points are in the
-% quarter-plane.  Only exception: we already are inside the quarter-plane.
+% quarter-square.  Only exception: we already are inside the quarter-square.
 checkTimes = [0 , whenReaches([x y], [cos(dir) sin(dir)], QP_v)];
 invalidX = 1 - isOnHalf(x + checkTimes * cos(dir), QP_v(1), QP_q(1));
 invalidY = 1 - isOnHalf(y + checkTimes * sin(dir), QP_v(2), QP_q(2));
@@ -25,7 +20,7 @@ invalidT = checkTimes < 0;
 inval = invalidX + invalidY + invalidT;
 
 for i = 1:length(checkTimes)
-    if inval(i) || checkTimes(i) > 20
+    if inval(i)
         checkTimes(i) = Inf;
     end
 end
@@ -33,13 +28,13 @@ end
 dist = min(checkTimes);
 
 if isnan(dist)
-    [x y dir]
+    [x y dir QP_v QP_q]
 end
 
 end
 
 function b = isOnHalf(u, v, q)
-    b = (u - v)*q >= -1e-9;
+    b = ((u - v)*q >= -1e-9) .* ((u - v)*q <= 1+1e-9);
 end
 
 function t = whenReaches(x, dx, v)
