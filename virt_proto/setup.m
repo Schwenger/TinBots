266e4a0d4a04e48befc66babf1e66ab4cf5abdf9
Marlene Boehmer <s9meboeh@stud.uni-saarland.de>
2016-06-01 15:34:39 +0200

    extended setup.m for matlab with creating workspace variable

--- virt_proto/setup.m
+++ virt_proto/setup.m
@@ -3,4 +3,9 @@ function setup()
     addpath('library');
     addpath('software');
     
-    load('proto.mat')
+    newData1 = load('proto.mat');
+    % Create new variables in the base workspace from those fields.
+    vars = fieldnames(newData1);
+    for i = 1:length(vars)
+        assignin('base', vars{i}, newData1.(vars{i}));
+    end
