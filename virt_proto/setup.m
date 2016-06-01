c5af0117ef93c8bbb2eda8da2133dbdb90a630ec
Maximilian Köhl <mail@koehlma.de>
2016-06-02 01:28:49 +0200

    [project][simulation] add test overview to library and open library on setup

--- virt_proto/setup.m
+++ virt_proto/setup.m
@@ -2,6 +2,9 @@ function setup()
     addpath('classes');
     addpath('library');
     addpath('software');
+    addpath('tests');
        
     disp(fileread('README.txt'));
+    
+    open('library/tinbot.slx');
 end
