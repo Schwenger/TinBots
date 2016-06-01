1522a05411a8e1206f379c8813afdd31717ccc72
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2016-06-01 15:45:09 +0200

    [project][software] Implement IR angle finder

---
+++ virt_proto/software/determine_victim_phi.m
@@ -0,0 +1,11 @@
+function victim_phi = determine_victim_phi(ir_start, ir_end, sensor_index)
+
+while ir_end < ir_start
+    ir_end = ir_end + 2*pi;
+end
+
+look_phi = (ir_start + ir_end) / 2;
+victim_phi = look_phi - (sensor_index - 1) * pi/3;
+victim_phi = mod(victim_phi, 2*pi);
+
+end
