2d20d3dd3e366bc6e71a2a2c1075a7ebdbdc9dd8
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2016-06-03 00:44:22 +0200

    victim_finder: sign error in my code
    Dangit :(

--- virt_proto/software/determine_victim_phi.m
+++ virt_proto/software/determine_victim_phi.m
@@ -5,7 +5,7 @@ while ir_end < ir_start
 end
 
 look_phi = (ir_start + ir_end) / 2;
-victim_phi = look_phi - (sensor_index - 1) * pi/3;
+victim_phi = look_phi + (sensor_index - 1) * pi/3;
 victim_phi = mod(victim_phi, 2*pi);
 
 end
