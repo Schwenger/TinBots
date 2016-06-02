c49f2b8f4d0bc7aa3c7c60da0d2049b3655defec
Maximilian Köhl <mail@koehlma.de>
2016-06-02 16:29:37 +0200

    [project][simulation] add left-right and raycast test cases

---
+++ virt_proto/tests/raycast_test.m
@@ -0,0 +1,5 @@
+function raycast_test()
+    map = TinMap('maps/left_right.txt', 10);
+    disp(TinUtils.raycast_polymorphic(map.matrix, 100, 100, -pi/2, false);
+    disp(TinUtils.raycast_polymorphic(map.matrix, 100, 100, pi/2, false);
+end
