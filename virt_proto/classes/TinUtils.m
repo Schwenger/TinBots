c6d4abf6f251edd4e1d658598c3824cf03b50af0
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2016-06-05 19:58:32 +0200

    fixup! path_finder: don't accidentally inflate into own position

--- virt_proto/classes/TinUtils.m
+++ virt_proto/classes/TinUtils.m
@@ -20,7 +20,8 @@ classdef TinUtils < handle
             % inflate walls with robot radius
             inflate(grid, 3);
             % Clear own position
-            grid(current_y, current_x) = 0;
+            % grid(floor(current_y), floor(current_x)) = 0;
+              % FIXME: Doesn't work, says "not enough input arguments"
             
             start_location = [floor(current_x) floor(current_y)];
             end_location = [floor(dst_x) floor(dst_y)];
