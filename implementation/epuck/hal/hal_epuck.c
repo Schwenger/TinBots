a7674ca4c5e66daecde269a0ad83a3b464e34fc9
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2016-06-22 18:22:23 +0200

    [implementation] Fix transition to own E-Puck lib

--- implementation/epuck/hal/hal_epuck.c
+++ implementation/epuck/hal/hal_epuck.c
@@ -6,10 +6,12 @@
 
 #include "hal.h"
 
+/*
 #include "e_uart_char.h"
 
 #include <motor_led/advance_one_timer/e_motors.h>
 #include <motor_led/advance_one_timer/e_led.h>
+*/
 
 void set_speed(double left, double right) {
     e_set_speed_left(left);
@@ -21,6 +23,8 @@ void set_led(unsigned int led, unsigned int value) {
 }
 
 void print(const char* message) {
+    /* FIXME
     e_send_uart1_char(message, strlen(message));
     while (e_uart1_sending());
+    */
 }
