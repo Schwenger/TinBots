9c9c7ed76b7295236e2f937b76bf81bd44822f4c
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2016-06-22 10:28:21 +0200

    [implementation] rhr: use constants, comments, implement

--- implementation/epuck/src/rhr.c
+++ implementation/epuck/src/rhr.c
@@ -24,8 +24,16 @@ enum RHR_STATES {
     RHR_run_close
 };
 
+static const double RHR_SENSE_TOL = 10;
+static const double RHR_MOTOR_MV = 2;
+static const double RHR_MOTOR_ROT = 1;
+static const double MV_PER_SEC = 2; /* FIXME ??? */
+static const double ROT_PER_SEC = MV_PER_SEC * 2 / 5.3;
+
 void rhr_reset(RhrLocals* rhr) {
     rhr->state = RHR_check_wall;
+    /* No further initialization needed because RHR_check_wall doesn't
+       read from rhr->time_entered */
 }
 
 static void find_wall(RhrLocals* rhr, Sensors* sens) {
@@ -42,7 +50,6 @@ static void find_wall(RhrLocals* rhr, Sensors* sens) {
     static const unsigned int order[NUM_PROXIMITY] =
         {4 - 1, 3 - 1, 5 - 1, 2 - 1, 6 - 1, 1 - 1, 7 - 1, 8 - 1};
     /* rot_angles = sense_angles - (-20); */
-    static const double threshold = 10;
     unsigned i;
 
     assert(sizeof(sense_angles) / sizeof(sense_angles[0])
@@ -53,7 +60,7 @@ static void find_wall(RhrLocals* rhr, Sensors* sens) {
     rhr->wall_p = 0;
     for(i = 0; i < 8; ++i) {
         unsigned int s = order[i];
-        if (sens->ir[s] <= threshold) {
+        if (sens->ir[s] <= RHR_SENSE_TOL) {
             rhr->wall_p = 1;
             rhr->wall_rot = sense_angles[s] - (-20.0*M_PI/180.0);
             return;
@@ -65,24 +72,35 @@ void rhr_step(RhrLocals* rhr, Sensors* sens) {
     const int old_state = rhr->state;
     switch (rhr->state) {
     case RHR_check_wall:
-        /* Don't change motor state, in case we're coming from find_wall */
         find_wall(rhr, sens);
         if (!rhr->wall_p) {
             rhr->state = RHR_find_wall;
+            /* Move forward */
+            set_speed(RHR_MOTOR_MV, RHR_MOTOR_MV);
         } else {
             if (rhr->wall_rot < 0) {
                 /* Rotate right */
-                set_speed(1, -1);
+                set_speed(RHR_MOTOR_ROT, -RHR_MOTOR_ROT);
                 rhr->wall_rot = -rhr->wall_rot;
             } else {
                 /* Rotate left */
-                set_speed(1, -1);
+                set_speed(-RHR_MOTOR_ROT, RHR_MOTOR_ROT);
             }
             rhr->state = RHR_wall_wait;
         }
         break;
     case RHR_find_wall:
-        /* FIXME */
+        /* -45, -20, +20, +45 degree are the sensors 2, 3, 4, 5 */
+        {
+            unsigned int i;
+            for (i = 2; i < 6; ++i) {
+                if (sens->proximity <= RHR_SENSE_TOL) {
+                    rhr->state = RHR_check_wall;
+                    /* Keep going (don't change motors) */
+                    break;
+                }
+            }
+        }
         break;
     case RHR_wall_wait:
         /* FIXME */
