1ee994204fa609d8a35cda6f3db180f52fb18b30
Maximilian Köhl <mail@koehlma.de>
2016-06-23 03:17:01 +0200

    [project][implementation] too tired to think about commit messages

--- implementation/epuck/main.c
+++ implementation/epuck/main.c
@@ -15,11 +15,21 @@ int main() {
 
     //tin_set_speed(500, 500);
 
+    char buffer[128];
+    unsigned int proximity[8];
+
     while (1) {
-        U2TXREG = 0x42;
-        while (!U2STAbits.TRMT);
-        U1TXREG = 0x42;
-        while (!U1STAbits.TRMT);
+        //U2TXREG = 0x42;
+        //while (!U2STAbits.TRMT);
+
+        tin_wait(500);
+
+        tin_get_proximity(proximity);
+        memset(buffer, 0, 128);
+        sprintf(buffer, "%lu %u\n", tin_get_time(), proximity[0]);
+        tin_com_print(buffer);
+
+        //while (tin_get_time() - start < 500);
     }
 }
 
