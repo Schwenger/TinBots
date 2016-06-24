402fb63c344d191a8d3e77df5063f0d9c3925593
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2016-06-24 12:44:56 +0200

    [implementation] Implement blind-cop state-machine

--- implementation/epuck/src/blind-cop.c
+++ implementation/epuck/src/blind-cop.c
@@ -6,9 +6,12 @@
 
 #include "blind-cop.h"
 
+static const hal_time NO_PATH_TIMEOUT = 40;
+
 enum BLIND_STATES {
     BLIND_init_rhr,
     BLIND_follow_path,
+    BLIND_pre_no_path,
     BLIND_no_path,
     BLIND_party
 };
@@ -47,66 +50,121 @@ static int time_passed_p(const hal_time entered, const hal_time wait_secs) {
     return entered - now >= wait_ticks;
 }
 
-void blind_reset(BlindLocals* blind) {
-    blind->state_big = BLIND_init_rhr;
-    blind->state_leaf = BLIND_LEAF_init_rhr;
+void blind_reset(BlindState* blind) {
+    blind->locals.state_big = BLIND_init_rhr;
+    blind->locals.state_leaf = BLIND_LEAF_init_rhr;
     /* No further initialization needed because BLIND_init_rhr doesn't
-       read from blind->time_entered */
+     * read from blind->time_entered */
+    blind->run_choice = BLIND_RUN_CHOICE_none;
+    /* No further initialization needed because dst_{x,y} should only
+     * be read for path_finder */
 }
 
-static void switch_failed(BlindLocals* blind, const char* msg) {
-    /* TODO: Candidate for code duplication (similar function alread in RHR) */
+static void switch_failed(BlindState* blind, const char* msg) {
     /* Uhh */
     hal_print(msg);
     assert(0);
     blind_reset(blind);
 }
 
-void blind_step(BlindInputs* inputs, BlindLocals* blind, Sensors* sens) {
-    const int old_state = blind->state_big;
-    switch (blind->state_big) {
+void blind_step(BlindInputs* inputs, BlindState* blind) {
+    const int old_state = blind->locals.state_big;
+    switch (blind->locals.state_big) {
     case BLIND_init_rhr:
-        switch (blind->state_leaf) {
-        case BLIND_LEAF_init_rhr:
-            /*FIXME*/
-            break;
-        case BLIND_LEAF_sense_angle:
-            /*FIXME*/
-            break;
-        case BLIND_LEAF_wait_angle_zero:
-            /*FIXME*/
-            break;
-        default:
-            switch_failed(blind, "BLIND/init_rhr illegal state?!  Resetting ...");
-            break;
+        if (inputs->found_victim_xy) {
+            blind->locals.state_big = BLIND_follow_path;
+            blind->locals.state_leaf = BLIND_LEAF_run_to_victim;
+            blind->run_choice = BLIND_RUN_CHOICE_none;
+            blind->dst_x = inputs->victim_x;
+            blind->dst_y = inputs->victim_y;
+        } else {
+            switch (blind->locals.state_leaf) {
+            case BLIND_LEAF_init_rhr:
+                blind->run_choice = BLIND_RUN_CHOICE_rhr;
+                if (inputs->need_angle) {
+                    blind->locals.state_leaf = BLIND_LEAF_sense_angle;
+                    blind->run_choice = BLIND_RUN_CHOICE_victim_finder;
+                }
+                break;
+            case BLIND_LEAF_sense_angle:
+                if (inputs->found_victim_phi) {
+                    blind->locals.state_leaf = BLIND_LEAF_wait_angle_zero;
+                    blind->run_choice = BLIND_RUN_CHOICE_none;
+                }
+                break;
+            case BLIND_LEAF_wait_angle_zero:
+                if (!inputs->found_victim_phi) {
+                    blind->locals.state_leaf = BLIND_LEAF_init_rhr;
+                    blind->run_choice = BLIND_RUN_CHOICE_rhr;
+                }
+                break;
+            default:
+                switch_failed(blind, "BLIND/init_rhr illegal state?!  Resetting ...");
+                break;
+            }
         }
         break;
     case BLIND_follow_path:
-        switch (blind->state_leaf) {
-        case BLIND_LEAF_run_to_victim:
-            /*FIXME*/
-            break;
-        case BLIND_LEAF_wait_path_zero:
-            /*FIXME*/
-            break;
-        default:
-            switch_failed(blind, "BLIND/follow_path illegal state?!  Resetting ...");
-            break;
+        if (inputs->no_path) {
+            blind->locals.state_big = BLIND_pre_no_path;
+            /* From here on, state_leaf must stay intact, so we can come back! */
+            blind->run_choice = BLIND_RUN_CHOICE_none;
+        } else {
+            switch (blind->locals.state_leaf) {
+            case BLIND_LEAF_run_to_victim:
+                /* Might be coming back from no_path */
+                blind->run_choice = BLIND_RUN_CHOICE_path_finder;
+                if (inputs->path_completed) {
+                    blind->locals.state_leaf = BLIND_LEAF_wait_path_zero;
+                    blind->run_choice = BLIND_RUN_CHOICE_none;
+                }
+                break;
+            case BLIND_LEAF_wait_path_zero:
+                if (!inputs->path_completed) {
+                    blind->locals.state_leaf = BLIND_LEAF_outta_here;
+                }
+                break;
+            case BLIND_LEAF_outta_here:
+                blind->run_choice = BLIND_RUN_CHOICE_path_finder;
+                blind->dst_x = inputs->origin_x;
+                blind->dst_y = inputs->origin_y;
+                if (inputs->path_completed) {
+                    blind->locals.state_big = BLIND_party;
+                    blind->run_choice = BLIND_RUN_CHOICE_none;
+                }
+                break;
+            default:
+                switch_failed(blind, "BLIND/follow_path illegal state?!  Resetting ...");
+                break;
+            }
+        }
+        break;
+    case BLIND_pre_no_path:
+        /* Just wait until the path_finder has reset. */
+        if (!inputs->no_path) {
+            blind->locals.state_big = BLIND_no_path;
+            blind->run_choice = BLIND_RUN_CHOICE_rhr;
         }
         break;
     case BLIND_no_path:
-        /*FIXME*/
+        if (time_passed_p(blind->locals.time_entered, NO_PATH_TIMEOUT)) {
+            blind->locals.state_big = BLIND_follow_path;
+            /* state_leaf should still be valid.
+             * Since we don't know which driver to run, go for 'none',
+             * and set it in the appropriate leaf state. */
+            blind->run_choice = BLIND_RUN_CHOICE_none;
+        }
         break;
     case BLIND_party:
-        /*FIXME*/
+        /* Nothing to do here. */
         break;
     default:
         switch_failed(blind, "blind illegal state?!  Resetting ...");
         break;
     }
 
-    /* only for BLIND_no_path */
-    if (blind->state_big != old_state) {
-        blind->time_entered = hal_get_time();
+    /* Only read by BLIND_no_path */
+    if (blind->locals.state_big != old_state) {
+        blind->locals.time_entered = hal_get_time();
     }
 }
--- implementation/epuck/src/blind-cop.h
+++ implementation/epuck/src/blind-cop.h
@@ -2,13 +2,13 @@
 #define BLIND_COP_H
 
 #include "hal.h"
-#include "sensors.h"
 
 typedef struct BlindInputs {
     int found_victim_phi;
     int found_victim_xy;
     int need_angle;
     int no_path;
+    int path_completed;
     double origin_x;
     double origin_y;
     double victim_x;
@@ -22,18 +22,20 @@ typedef struct BlindLocals {
 } BlindLocals;
 
 enum BlindRunChoice {
+    BLIND_RUN_CHOICE_none,
     BLIND_RUN_CHOICE_path_finder,
     BLIND_RUN_CHOICE_rhr,
     BLIND_RUN_CHOICE_victim_finder
 };
 
-typedef struct BlindOutputs {
+typedef struct BlindState {
+    BlindLocals locals;
     double dst_x;
     double dst_y;
     enum BlindRunChoice run_choice;
-} BlindOutputs;
+} BlindState;
 
-void blind_reset(BlindLocals* rhr);
-void blind_step(BlindInputs* inputs, BlindLocals* rhr, Sensors* sens);
+void blind_reset(BlindState* blind);
+void blind_step(BlindInputs* inputs, BlindState* blind);
 
 #endif
