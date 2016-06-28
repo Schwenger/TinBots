04cb8da284080699a611a6d02ebe5b572675a74d
Maximilian Köhl <mail@koehlma.de>
2016-06-28 02:45:44 +0200

    [project][implementation] working bluetooth interrupts

--- implementation/epuck/main.c
+++ implementation/epuck/main.c
@@ -7,6 +7,8 @@
 
 #include "tinpuck.h"
 
+extern unsigned char com_buffer[64];
+
 int main() {
     tin_init();
 
@@ -26,15 +28,17 @@ int main() {
 
         tin_get_proximity(proximity);
 
-        for (index = 0; index < 8; index++) {
+        /*for (index = 0; index < 8; index++) {
             tin_set_led(index, proximity[index] > 15 ? ON : OFF);
-        }
+        }*/
 
         memset(buffer, 0, 128);
-        sprintf(buffer, "%lu %u %u %u %u %u %u %u %u \n", tin_get_time(),
+        sprintf(buffer, "%lu %u %u %u %u %u %u %u %u\n", tin_get_time(),
                 proximity[0], proximity[1], proximity[2], proximity[3],
                 proximity[4], proximity[5], proximity[6], proximity[7]);
-        tin_com_print("====");
+        tin_com_print(buffer);
+        memset(buffer, 0, 128);
+        sprintf(buffer, "%s\n", com_buffer);
         tin_com_print(buffer);
     }
 }
