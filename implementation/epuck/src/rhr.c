5ca1ccd899a657fe44440a56956b227283642914
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2016-06-30 14:26:20 +0200

    [implementation] rhr: d slightly too small, x way too small

--- implementation/epuck/src/rhr.c
+++ implementation/epuck/src/rhr.c
@@ -25,8 +25,8 @@ enum RHR_STATES {
 typedef char check_rhr_states_size[
     (sizeof(enum RHR_STATES) == sizeof(int)) ? 1 : -1];
 
-static const double RHR_CONF_CORNER_D = 11;
-static const double RHR_CONF_CORNER_X = 6;
+static const double RHR_CONF_CORNER_D = 10;
+static const double RHR_CONF_CORNER_X = 10;
 static const double RHR_CONF_WALL_THRESH = 1.5;
 static const double RHR_CONF_WALL_D = 1;
 
