c0cac2b31d478692fb5689b498d89cfaaf6ed402
Maximilian Schwenger <schwengermaximilian@gmail.com>
2016-06-30 16:12:34 +0200

    Implemented path finder state machine.

--- implementation/epuck/map.h
+++ implementation/epuck/map.h
@@ -1,6 +1,8 @@
 #ifndef EPUCK_MAP_H
 #define EPUCK_MAP_H
 
+#include "pi.h"
+
 typedef struct Map {
     const int width; /* byte probably */
     const int height;
@@ -22,6 +24,14 @@ enum {
     FIEL_UNKOWN
 };
 
+Position map_discretize(Map* map, double x, double y);
 void send_map(Map* map, Position center, int radius);
+
+Position map_discretize(Map* map, double x, double y) {
+    Position res;
+    res.x = (int) (floor(x / map->width));
+    res.y = (int) (floor(y / map->height));
+    return res;
+}
 
 #endif /* EPUCK_MAP_H */
--- implementation/epuck/src/path-finder.h
+++ implementation/epuck/src/path-finder.h
@@ -1,11 +1,36 @@
 #ifndef EPUCK_PATHFINDER_H
 #define EPUCK_PATHFINDER_H
 
+#include <hal/hal.h>
 #include "map.h"
+#include "sensors.h"
 
 #define STEP_DISTANCE 4
 #define MAX_PATH_LENGTH 20 /* map size over const dist */
 /* Caller must allocate space for MAX_PATH_LENGTH+1 instances of Position. */
-void pf_find_path(Position position, Position goal, Map *map, Position *path);
+
+typedef struct PathFinderInputs {
+    double dest_x;
+    double dest_y;
+    int compute;
+} PathFinderInputs;
+
+typedef struct PathFinderLocals {
+    Position path[MAX_PATH_LENGTH + 1];
+    int path_index;
+    Map* map;
+} PathFinderLocals;
+
+typedef struct PathFinderState {
+    int state;
+    PathFinderLocals locals;
+    int drive;
+    Position next;
+    int nopath;
+    int path_completed;
+} PathFinderState;
+
+void pf_reset(PathFinderState* pf);
+void pf_step(PathFinderInputs* inputs, PathFinderState* pf, Sensors* sens);
 
 #endif /*EPUCK_PATHFINDER_H*/
