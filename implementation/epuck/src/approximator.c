e8b482adf46dbed1346da329add7248218187ecb
Marlene Boehmer <s9meboeh@stud.uni-saarland.de>
2016-07-06 15:56:42 +0200

    fixed wrong use of time in approximator and indicate received lps signal

--- implementation/epuck/src/approximator.c
+++ implementation/epuck/src/approximator.c
@@ -19,9 +19,13 @@ static const double magic_number_2 = 5.3;
 void approx_step(ApproxInputs* inputs, ApproxState* approx) {
     ApproxLocals* loc = &approx->locals;
     hal_time delta_t;
+    hal_time next_t;
     double delta_phi, v, p;
+    static unsigned int status = 0;
 
     if(inputs->lps->x != -1){
+        status ^= 1;
+        hal_set_front_led(status);
         loc->x = inputs->lps->x;
         loc->y = inputs->lps->y;
         loc->phi = inputs->lps->phi;
@@ -30,18 +34,19 @@ void approx_step(ApproxInputs* inputs, ApproxState* approx) {
     } else {
         v = (loc->prev_motor_left + loc->prev_motor_right) / magic_number_1;
         p = (loc->prev_motor_right - loc->prev_motor_left) / magic_number_2;
-        delta_t = hal_get_time() - loc->prev_t;
+        next_t = hal_get_time() / 1000.0;
+        delta_t = next_t - loc->prev_t;
         delta_phi = p * delta_t;
         if(loc->prev_motor_right == loc->prev_motor_left){ /* i.e. p == 0 */
-            loc->x = v * cos(loc->phi) * delta_t + loc->x;
-            loc->y = v * sin(loc->phi) * delta_t + loc->y;
+            loc->x += v * cos(loc->phi) * delta_t;
+            loc->y += v * sin(loc->phi) * delta_t;
         } else {
             loc->x += v * (sin(delta_phi + loc->phi) - sin(loc->phi)) / p;
             loc->y += v * (cos(loc->phi) - cos(delta_phi + loc->phi)) / p;
         }
         loc->phi = fmod(loc->phi + p * delta_t, 2 * M_PI);
     }
-    loc->prev_t = hal_get_time();
+    loc->prev_t = next_t;
     loc->prev_motor_left = inputs->motor_left;
     loc->prev_motor_right = inputs->motor_right;
     approx->current.x = loc->x;
