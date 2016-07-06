7b3c6afe03f3e6dcbfb55ba29ecc355f78535160
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2016-07-07 00:35:56 +0200

    [implementation] tests: unit-test victim-finder

---
+++ implementation/epuck/tests/src/vicfin.c
@@ -0,0 +1,58 @@
+#include <assert.h>
+#include <stdio.h>
+
+#include "pi.h"
+#include "stub.h"
+#include "victim-finder.h"
+
+typedef struct Measurement {
+    double x, y, phi;
+} Measurement;
+
+typedef struct Test {
+    Measurement m1;
+    Measurement m2;
+    struct {double x; double y;} res;
+} Test;
+
+int main() {
+    VFState vfs;
+    VFInputs in;
+    Sensors sens;
+    const Test the_test = {
+        {45, 50, 0*M_PI/2}, {50, 45, 1*M_PI/2}, {50, 50}
+    };
+
+    vf_reset(&vfs);
+
+    in.found_victim_phi = 1;
+    sens.current.x = the_test.m1.x;
+    sens.current.y = the_test.m1.y;
+    in.victim_angle = the_test.m1.phi;
+    vf_step(&in, &vfs, &sens);
+    assert(!vfs.found_victim_xy);
+    in.found_victim_phi = 0;
+    vf_step(&in, &vfs, &sens);
+    assert(!vfs.found_victim_xy);
+
+    in.found_victim_phi = 1;
+    sens.current.x = the_test.m2.x;
+    sens.current.y = the_test.m2.y;
+    in.victim_angle = the_test.m2.phi;
+    vf_step(&in, &vfs, &sens);
+    assert(vfs.found_victim_xy);
+    in.found_victim_phi = 0;
+    vf_step(&in, &vfs, &sens);
+    assert(vfs.found_victim_xy);
+    printf("Actual %.1f,%.1f expected %.1f,%.1f\n",
+        vfs.victim_x, vfs.victim_y, the_test.res.x, the_test.res.y);
+    if (fabs(vfs.victim_x - the_test.res.x) < 0.01
+        || fabs(vfs.victim_y - the_test.res.y) < 0.01) {
+        printf("\t=> and that's GOOD!\n");
+    } else {
+        printf("\t=> and that's BAD!\n");
+        return -1;
+    }
+
+    return 0;
+}
