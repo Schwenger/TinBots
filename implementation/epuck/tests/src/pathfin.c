4af1646431497d7df95792ed02e309dfa604615a
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2016-07-07 15:49:06 +0200

    [implementation] tests: unit test for path-finder

---
+++ implementation/epuck/tests/src/pathfin.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h> /* malloc, free */
+
+#include "path-finder.h"
+
+int main(void){
+	const int size = 10;
+	int i, j;
+	int** occ;
+    Position start, goal;
+    Map map;
+    Position* path;
+
+    /* Build a map */
+    occ = (int**) malloc(size * sizeof(int*));
+	for(i = 0; i < size; i++) {
+		occ[i] = (int*) malloc(size * sizeof(int));
+		for(j = 0; j < size; ++j)
+			occ[i][j] = 0;
+	}
+	for(i = 0; i < 8; ++i)
+		occ[3][i] = 1;
+
+	start.x = 0;
+    start.y = 0;
+	goal.x = 9;
+    goal.y = 9;
+	map.width = size;
+    map.height = size;
+    map.occupancy = occ;
+
+	printf("Starting search\n");
+	path = (Position*) malloc((MAX_PATH_LENGTH + 1) * sizeof(Position));
+	pf_find_path(start, goal, &map, path);
+
+    printf("Found path:\n(%d, %d)", start.x, start.y);
+    for (i = 0; i < MAX_PATH_LENGTH && path[i].x != -1 && path[i].y != -1; ++i) {
+		printf("-> (%d, %d)", path[i].x, path[i].y);
+	}
+    printf("\nAdditional data:\n");
+    for (; i < MAX_PATH_LENGTH; ++i) {
+		printf("-> (%d, %d)", path[i].x, path[i].y);
+	}
+    printf("\n\tEVALUATE BY HAND!\n");
+
+	for(i = 0; i < size; i++) {
+		free(occ[i]);
+	}
+    free(occ);
+    free(path);
+
+	return 0;
+}
