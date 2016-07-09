8ce0673c79fb23670881fdcc9bfb05bc1718972d
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2016-07-10 01:00:16 +0200

    [implementation] stubs for T2T handling

---
+++ implementation/epuck/src/t2t.c
@@ -0,0 +1,40 @@
+#include "t2t.h"
+
+/* Again, these are supposed to run within ISRs, so be quick about
+ * copying these things into bot->* structures! */
+
+void t2t_receive_heartbeat(TinBot* bot) {
+    /* FIXME */
+    (void)bot;
+}
+
+void t2t_receive_found_phi(TinBot* bot, int x, int y, double phi) {
+    /* FIXME */
+    (void)bot;
+    (void)x;
+    (void)y;
+}
+
+void t2t_receive_found_xy(TinBot* bot, int is_ours, int x, int y, int iteration) {
+    /* FIXME */
+    (void)bot;
+    (void)x;
+    (void)y;
+}
+
+void t2t_receive_update_map(TinBot* bot, int is_ours, int x, int y, unsigned char* map_data) {
+    /* FIXME */
+    (void)bot;
+    (void)x;
+    (void)y;
+}
+
+void t2t_receive_docked(TinBot* bot) {
+    /* FIXME */
+    (void)bot;
+}
+
+void t2t_receive_completed(TinBot* bot) {
+    /* FIXME */
+    (void)bot;
+}
---
+++ implementation/epuck/src/t2t.h
@@ -0,0 +1,38 @@
+#ifndef EPUCK_T2T_H
+#define EPUCK_T2T_H
+
+#include "tinbot.h"
+
+/* All of the following function are intended to be run from withing an ISR.
+ * Please make sure they are only called when the package is from the
+ * right source! */
+
+/* 'Our' packages: MAY (don't care)
+ * 'Their' packages: MUST */
+void t2t_receive_heartbeat(TinBot* bot);
+
+/* 'Our' packages: MUST NOT
+ * 'Their' packages: MUST */
+void t2t_receive_found_phi(TinBot* bot, int x, int y, double phi);
+
+/* 'Our' packages: MUST
+ * 'Their' packages: MUST
+ * Please set the 'is_ours' flag. */
+void t2t_receive_found_xy(TinBot* bot, int is_ours, int x, int y, int iteration);
+
+/* 'Our' packages: MUST
+ * 'Their' packages: MUST
+ * Please set the 'is_ours' flag. */
+void t2t_receive_update_map(TinBot* bot, int is_ours, int x, int y, unsigned char* map_data);
+
+/* 'Our' packages: MUST NOT
+ * 'Their' packages: MUST */
+void t2t_receive_docked(TinBot* bot);
+
+/* 'Our' packages: MAY (don't care)
+ * 'Their' packages: MAY (don't care)
+ * Actually, I don't see any reasonable way we could
+ * ever *receive* that command. */
+void t2t_receive_completed(TinBot* bot);
+
+#endif
